bd768a4f2ac784c3ecc14ff27761a55584db444b
phuoc <phuoc.anonydev2k3@gmail.com>
2024-08-28 17:55:21 +0700

    f

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Area,
@@ -17,7 +17,7 @@ import {
   ReferenceLine,
   XAxis,
   YAxis,
-} from "recharts"
+} from "recharts";
 
 import {
   Card,
@@ -26,21 +26,19 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components//ui/card"
+} from "@/components//ui/card";
 import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components//ui/chart"
-import { Separator } from "@/components//ui/separator"
+} from "@/components//ui/chart";
+import { Separator } from "@/components//ui/separator";
 
 export function Charts() {
   return (
     <div className="chart-wrapper mx-auto flex max-w-6xl flex-col flex-wrap items-start justify-center gap-6 p-6 sm:flex-row sm:p-8">
       <div className="grid w-full gap-6 sm:grid-cols-2 lg:max-w-[22rem] lg:grid-cols-1 xl:max-w-[25rem]">
-        <Card
-          className="lg:max-w-md" x-chunk="charts-01-chunk-0"
-        >
+        <Card className="lg:max-w-md" x-chunk="charts-01-chunk-0">
           <CardHeader className="space-y-0 pb-2">
             <CardDescription>Today</CardDescription>
             <CardTitle className="text-4xl tabular-nums">
@@ -111,7 +109,7 @@ export function Charts() {
                   tickFormatter={(value) => {
                     return new Date(value).toLocaleDateString("en-US", {
                       weekday: "short",
-                    })
+                    });
                   }}
                 />
                 <ChartTooltip
@@ -124,7 +122,7 @@ export function Charts() {
                           day: "numeric",
                           month: "long",
                           year: "numeric",
-                        })
+                        });
                       }}
                     />
                   }
@@ -166,9 +164,7 @@ export function Charts() {
             </CardDescription>
           </CardFooter>
         </Card>
-        <Card
-          className="flex flex-col lg:max-w-md" x-chunk="charts-01-chunk-1"
-        >
+        <Card className="flex flex-col lg:max-w-md" x-chunk="charts-01-chunk-1">
           <CardHeader className="flex flex-row items-center gap-4 space-y-0 pb-2 [&>div]:flex-1">
             <div>
               <CardDescription>Resting HR</CardDescription>
@@ -252,7 +248,7 @@ export function Charts() {
                   tickFormatter={(value) => {
                     return new Date(value).toLocaleDateString("en-US", {
                       weekday: "short",
-                    })
+                    });
                   }}
                 />
                 <Line
@@ -277,7 +273,7 @@ export function Charts() {
                           day: "numeric",
                           month: "long",
                           year: "numeric",
-                        })
+                        });
                       }}
                     />
                   }
@@ -289,13 +285,11 @@ export function Charts() {
         </Card>
       </div>
       <div className="grid w-full flex-1 gap-6 lg:max-w-[20rem]">
-        <Card
-          className="max-w-xs" x-chunk="charts-01-chunk-2"
-        >
+        <Card className="max-w-xs" x-chunk="charts-01-chunk-2">
           <CardHeader>
             <CardTitle>Progress</CardTitle>
             <CardDescription>
-              You're average more steps a day this year than last year.
+              You average more steps a day this year than last year.
             </CardDescription>
           </CardHeader>
           <CardContent className="grid gap-4">
@@ -403,9 +397,7 @@ export function Charts() {
             </div>
           </CardContent>
         </Card>
-        <Card
-          className="max-w-xs" x-chunk="charts-01-chunk-3"
-        >
+        <Card className="max-w-xs" x-chunk="charts-01-chunk-3">
           <CardHeader className="p-4 pb-0">
             <CardTitle>Walking Distance</CardTitle>
             <CardDescription>
@@ -487,9 +479,7 @@ export function Charts() {
             </ChartContainer>
           </CardContent>
         </Card>
-        <Card
-          className="max-w-xs" x-chunk="charts-01-chunk-4"
-        >
+        <Card className="max-w-xs" x-chunk="charts-01-chunk-4">
           <CardContent className="flex gap-4 p-4 pb-2">
             <ChartContainer
               config={{
@@ -596,9 +586,7 @@ export function Charts() {
         </Card>
       </div>
       <div className="grid w-full flex-1 gap-6">
-        <Card
-          className="max-w-xs" x-chunk="charts-01-chunk-5"
-        >
+        <Card className="max-w-xs" x-chunk="charts-01-chunk-5">
           <CardContent className="flex gap-4 p-4">
             <div className="grid items-center gap-2">
               <div className="grid flex-1 auto-rows-min gap-0.5">
@@ -686,13 +674,11 @@ export function Charts() {
             </ChartContainer>
           </CardContent>
         </Card>
-        <Card
-          className="max-w-xs" x-chunk="charts-01-chunk-6"
-        >
+        <Card className="max-w-xs" x-chunk="charts-01-chunk-6">
           <CardHeader className="p-4 pb-0">
             <CardTitle>Active Energy</CardTitle>
             <CardDescription>
-              You're burning an average of 754 calories per day. Good job!
+              You burning an average of 754 calories per day. Good job!
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-row items-baseline gap-4 p-4 pt-2">
@@ -769,9 +755,7 @@ export function Charts() {
             </ChartContainer>
           </CardContent>
         </Card>
-        <Card
-          className="max-w-xs" x-chunk="charts-01-chunk-7"
-        >
+        <Card className="max-w-xs" x-chunk="charts-01-chunk-7">
           <CardHeader className="space-y-0 pb-0">
             <CardDescription>Time in Bed</CardDescription>
             <CardTitle className="flex items-baseline gap-1 text-4xl tabular-nums">
@@ -877,5 +861,5 @@ export function Charts() {
         </Card>
       </div>
     </div>
-  )
+  );
 }
