2fb7e7923accebc71dcf7d1102cb86e7d4642d8b
phuoc <phuoc.anonydev2k3@gmail.com>
2024-08-28 17:01:03 +0700

    f

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,9 @@ export default function Home() {
         console.log(liff.getAccessToken());
         console.log(liff.getOS()); // Available
         console.log(liff.getAppLanguage()); // Available
+
         !liff.isLoggedIn() && liff.login(); // Not available
+        liff.isLoggedIn() && liff.logout();
       });
   }, []);
   return (
